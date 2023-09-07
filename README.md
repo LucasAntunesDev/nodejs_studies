a84a1912655393442cf77657c2ea9f3f04dc7a7a
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-09-07 08:45:46 -0300

    add: usando node.js com fastify

---
+++ README.md
@@ -0,0 +1,2 @@
+# Node.j Studies
+Repositório para armazenar os códigos que estou desenvolvendo enquanto aprendo o básico do Node.js
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-import { createServer } from 'node:http'
+/*import { createServer } from 'node:http'
+*
+*const server = createServer((req, res) => {
+*   console.log('Olá mundo!')
+*   //Escrevendo uma mensagem na tela
+*   res.write('Olá mundo!')
+*   return res.end()
+*})
+*
+* //Porta em que vou rodar o servidor
+*server.listen(3333)*/
 
-const server = createServer((req, res) => {
-    console.log('Olá mundo!')
-    //Escrevendo uma mensagem na tela
-    res.write('Olá mundo!')
-    return res.end()
+import { fastify } from 'fastify'
+
+const server = fastify()
+
+server.get('/', () => {
+    return 'Olá mundo!'
 })
 
-//Porta em que vou rodar o servidor
-server.listen(3333)
+server.listen({
+    port:3333,
+})
